1ded0b8e2ea307d8d7d67fd727e7d5fd7c206703
Rodolfo Guluarte Hale <guluarte@gmail.com>
2024-09-09 12:11:06 -0700

    Time: 73 ms (9.99%) | Memory: 53.3 MB (5.38%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    let dummy = {
+        next: null
+    };
+
+    const first = dummy
+
+    let [head1, head2] = [list1, list2]
+
+    while (head1 && head2) {
+        if (head1.val < head2.val) {
+            dummy.next = head1
+            head1 = head1.next
+        } else {
+            dummy.next = head2
+            head2 = head2.next
+        }
+
+        dummy = dummy.next
+    }
+
+    while (head1) {
+        dummy.next = head1
+        head1 = head1.next
+        dummy = dummy.next
+    }
+
+    while (head2) {
+        console.log(dummy)
+        dummy.next = head2
+        head2 = head2.next
+        dummy = dummy.next
+    }
+    return first.next
+
+
+};
