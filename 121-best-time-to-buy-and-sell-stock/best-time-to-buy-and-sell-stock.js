9eec4ffa3a1c7742a54760e202046c415297f76b
Rodolfo Guluarte Hale <guluarte@gmail.com>
2024-09-09 12:32:08 -0700

    Time: 72 ms (66.96%) | Memory: 60 MB (13.05%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxPrice = 0
+    let l = 0
+
+    for(let r = 0; r < prices.length; r++) {
+        const sellPrice = prices[r] - prices[l]
+        maxPrice = Math.max(maxPrice, sellPrice)
+        if(prices[r] < prices[l]) {
+            l = r
+        }
+    }
+
+    return maxPrice
+};
