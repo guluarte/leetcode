980583522862f6425e1a9e5fe020d330bf9fade0
Rodolfo Guluarte Hale <guluarte@gmail.com>
2024-09-09 15:59:31 -0700

    Time: 76 ms (39.59%) | Memory: 51.3 MB (77.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,25 +3,22 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-   if(s.length !== t.length) return false
-   const charCounts = {}
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) return false
+    const charCounts = {}
 
-   for(let i = 0; i < s.length; i++) {
-    const sL = s[i]
-    const tL = t[i]
-    charCounts[sL] = charCounts[s[i]] || 0
-    charCounts[tL] = charCounts[t[i]] || 0
-    charCounts[sL]++
-    charCounts[tL]--
-   }
+    for (let i = 0; i < s.length; i++) {
+        const sL = s[i]
+        const tL = t[i]
+        charCounts[sL] = charCounts[sL] || 0
+        charCounts[tL] = charCounts[tL] || 0
+        charCounts[sL]++
+        charCounts[tL]--
+    }
 
-   for(letter of Object.values(charCounts)) {
-    console.log(letter)
-    if(letter !== 0) return false
-   }
+    for (letter of Object.values(charCounts)) {
+        if (letter !== 0) return false
+    }
 
-   console.log(charCounts)
-
-   return true
+    return true
 };
