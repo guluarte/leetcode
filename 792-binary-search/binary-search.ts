5d84a4718d746816bae305d787885287f1484194
Rodolfo Guluarte Hale <guluarte@gmail.com>
2024-10-18 11:16:18 -0700

    Time: 0 ms (100.00%) | Memory: 54.2 MB (7.61%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -5,8 +5,7 @@ function search(nums: number[], target: number): number {
     while (low < high) {
         let mid = low + Math.floor((high - low) / 2)
         let answer = nums[mid]
-        console.log({ low, mid, high, answer, target }
-        )
+
         if (answer === target) {
             return mid
         }
