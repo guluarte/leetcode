52d74d3da81c9a7de601a84a44af566a090e00ba
Rodolfo Guluarte Hale <guluarte@gmail.com>
2024-11-28 19:46:37 -0700

    Time: 8 ms (92.43%) | Memory: 59.8 MB (98.40%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -0,0 +1,16 @@
+function containsDuplicate(nums: number[]): boolean {
+        let dups = {};
+    
+    for(let i = 0; i < nums.length; i++) {
+      const bucket = Math.floor(nums[i] / 32);
+      const x =  1 << nums[i];
+      const num = x;
+      
+      if(num & dups[bucket]) return true;
+      
+      dups[bucket] = num | dups[bucket];
+
+    }
+
+    return false;
+};
